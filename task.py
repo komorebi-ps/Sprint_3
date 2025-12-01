5016e1da600e6d2c93228353db902c06b20ddac6
Polina Spiridonova <tequitela@gmail.com>
2025-11-30 18:27:31 -0800

    Исправления в обработке исключений +исправлены отступы

--- task.py
+++ task.py
@@ -19,19 +19,19 @@ class OnlineSalesRegisterCollector:
     
 #2
     def add_item_to_cheque(self, name):
-            if len(name) == 0 or len(name) > 40:
-                raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
-            if name not in self.__item_price:
-                raise NameError('Позиция отсутствует в товарном справочнике')
-            self.__name_items.append(name)
-            self.__number_items += 1
+        if len(name) == 0 or len(name) > 40:
+            raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+        if name not in self.__item_price:
+            raise NameError('Позиция отсутствует в товарном справочнике')
+        self.__name_items.append(name)
+        self.__number_items += 1
 
  #3                                   
     def delete_item_from_check(self, name):
-            if name not in self.__name_items:
-                raise NameError('Позиция отсутствует в чеке')
-            self.__name_items.remove(name)
-            self.__number_items -= 1
+        if name not in self.__name_items:
+            raise NameError('Позиция отсутствует в чеке')
+        self.__name_items.remove(name)
+        self.__number_items -= 1
 
 #4
     def check_amount(self):
